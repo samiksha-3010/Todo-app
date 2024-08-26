6f41d541a4e69308827db9d92780a54191e8833c
samiksha-3010 <samiksha30102@gmail.com>
2024-08-26 15:47:31 +0530

    done

--- server/middlewares/TokenMiddleware.js
+++ server/middlewares/TokenMiddleware.js
@@ -1,19 +1,19 @@
-// import Jwt from 'jsonwebtoken'
+import Jwt from 'jsonwebtoken'
 
-// export const isJwtExpire = async (req, res, next) => {
-//     try {
-//         const { token } = req.body;
+export const isJwtExpire = async (req, res, next) => {
+    try {
+        const { token } = req.body;
 
-//         const decodedData = await Jwt.verify(token, process.env.JWT_SECRET);
-//         const getCurrentUser = Math.floor(Date.now() / 1000)
+        const decodedData = await Jwt.verify(token, process.env.JWT_SECRET);
+        const getCurrentUser = Math.floor(Date.now() / 1000)
 
-//         if(decodedData?.exp <= getCurrentUser){
-//             return res.status(404).json({success : false,  tokenExpire : true, message : "Token is expired, Please login again." })
-//         }
+        if(decodedData?.exp <= getCurrentUser){
+            return res.status(404).json({success : false,  tokenExpire : true, message : "Token is expired, Please login again." })
+        }
 
-//         next();
+        next();
 
-//     } catch (error) {
-//         return res.status(500).json({ success: false, error: error })
-//     }
-// }
+    } catch (error) {
+        return res.status(500).json({ success: false, error: error })
+    }
+}
